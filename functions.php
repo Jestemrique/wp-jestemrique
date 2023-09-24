7af8d5e82f02f934a5c4ce9fd6d08f8da132ce28
Enrique <enrique.pastrana@gmail.com>
2023-09-24 17:29:42 +0200

    updated funtions.php added assets/js/bootstrap.js

--- functions.php
+++ functions.php
@@ -8,7 +8,8 @@ function custom_theme_assets() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 	wp_enqueue_style( 'epr-styles', get_stylesheet_directory_uri().'/epr-styles.css' );
 
-	wp_enqueue_script( 'bootstrapjs', get_template_directory_uri()  . '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', array(), false, array('strategy'  => 'defer',) );
+	//wp_enqueue_script( 'bootstrapjs', get_template_directory_uri()  . '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', array(), false, array('strategy'  => 'defer',) );
+	wp_enqueue_script( 'bootstrapjs', get_template_directory_uri()  . '/assets/js/bootstrap.bundle.min.js', array(), false, array('strategy'  => 'defer',) );
 }
 
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
